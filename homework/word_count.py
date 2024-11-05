a27f37c4922209a6d5fe09785406a2d36ee52725
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2024-11-05 10:55:50 -0500

    Initial commit

---
+++ homework/word_count.py
@@ -0,0 +1,16 @@
+"""Taller evaluable"""
+
+
+#
+# Escriba la función job, la cual orquesta las funciones anteriores.
+#
+def run_job(input_directory, output_directory):
+    """Job"""
+
+
+if __name__ == "__main__":
+
+    run_job(
+        "files/input",
+        "files/output",
+    )
